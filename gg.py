8d80b886019966f366af6653e5023fcacaad1db2
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-07 13:45:50 +0530

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, Filters
 
+app = Client("session",bot_token="511016924:AAHHmS_NafrmUawma9PpfRueQUEFAOY7BJw",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
 
 @app.on_message(Filters.private)
 def main(client, message):
